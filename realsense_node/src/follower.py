866f9ed5bb9bd4cd77df6e5861f30ab1a6c4db2d
Aryans006 <aryansethi@duck.com>
2025-06-27 17:05:24 +0530

    "added depth detection"

--- realsense_node/src/follower.py
+++ realsense_node/src/follower.py
@@ -11,6 +11,7 @@ class MyNode(Node):
     def __init__(self):
         super().__init__('image_display_node')
         
+        self.model = YOLO("yolov8n.pt")  
         self.br = CvBridge()
         self.frame = None  # ← Store latest frame here
         
@@ -19,39 +20,69 @@ class MyNode(Node):
             '/camera/camera/color/image_raw',  # Change topic name if needed
             self.listener_callback, 
             10)
+        
+        self.depth_frame = None
+        self.depth_sub = self.create_subscription(
+            Image,
+            '/camera/camera/depth/image_rect_raw',
+            self.depth_callback,
+            10)
 
         # Timer to display image at 30 Hz
-        self.timer = self.create_timer(1/30.0, self.display_image)
+        self.timer = self.create_timer(1/50.0, self.display_image)
+        # self.timer2 = self.create_timer(1/30.0, self.display_image_Depth)
 
     def listener_callback(self, msg):
         try:
+            print("entered normal callback")
             self.frame = self.br.imgmsg_to_cv2(msg, desired_encoding='bgr8')
         except Exception as e:
             self.get_logger().error(f"Failed to convert image: {e}")
+            
+    def depth_callback(self, msg):
+        try:
+            print("entered depth callback")
+            self.depth_frame = self.br.imgmsg_to_cv2(msg, desired_encoding='passthrough')
+        except Exception as e:
+            self.get_logger().error(f"Failed to convert image: {e}")
         
+
+
     def display_image(self):
-        if self.frame is not None:
+        
+        if self.frame is None or self.depth_frame is None:
+            return
+        
+        if self.frame is not None and self.depth_frame is not None:
             
-            model = YOLO("yolov8n.pt")  
             img = self.frame
-            results = model(img)[0]
+            results = self.model(img)[0]
             
+        if self.frame.shape[:2] != self.depth_frame.shape[:2]:
+            self.get_logger().warn("RGB and Depth resolutions don't match!")
+            
+        else:    
             for box in results.boxes:
+                print("leaving now")
                 if int(box.cls[0]) == 39:  # class 0 = person
+                    print("here now")
                     x1, y1, x2, y2 = map(int, box.xyxy[0])
                     conf = box.conf[0]
                     cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
-                    cx = (x1 + x2) / 2
-                    cy = (y1 + y2) / 2
-                    cv2.circle(img, (int(cx),int(cy)), 1, (0,0,255), -1 )
+                    self.cx = (x1 + x2) / 2
+                    self.cy = (y1 + y2) / 2
+                    cv2.circle(img, (int(self.cx),int(self.cy)), 1, (0,0,255), -1 )
                     cv2.putText(img, f'bottle {conf:.2f}', (x1, y1 - 10),
                                 cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+                    print("here now")
+                    depth_val = self.depth_frame[int(self.cy), int(self.cx)]
+                    print("DEPTH OF POINT", depth_val)
 
-            # Show result
-            cv2.imshow("Detection", img)
-            key = cv2.waitKey(1)
-            if key == ord('q'):
-                rclpy.shutdown()
+                    # Show result
+                    cv2.imshow("Detection", img)
+                    key = cv2.waitKey(1)
+                    if key == ord('q'):
+                        rclpy.shutdown()
 
 
 def main(args=None):
